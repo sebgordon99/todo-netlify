1cc54e17ccb5b3f2f0e8a2d7cf21bfdd3e13752a
Sebastian Gordon <sebgordon99@gmail.com>
2026-01-05 11:22:32 +1100

    similar removal

--- netlify/functions/routes/index.js
+++ netlify/functions/routes/index.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import todos from "./todos.js";
 import ads from "./ads.js";
-import availabilitys from "./availabilitys.js";
+import availability from "./availability.js";
 import locations from "./locations.js";
 import instruments from "./instruments.js";
 import users from "./users.js";
@@ -11,7 +11,7 @@ const router = Router();
 
 router.use("/todos", todos);
 router.use("/ads", ads);
-router.use("/availabilitys", availabilitys);
+router.use("/availability", availability);
 router.use("/locations", locations);
 router.use("/instruments", instruments);
 router.use("/users", users);
